1d2dcda833404749bcf341764c4853de9e09052f
Nathan Isaac <nathan@nathan-isaac.com>
2015-02-15 22:43:27 -0800

    Add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Nathan Isaac Portfolio</title>
 
+    <link rel="apple-touch-icon" href="/images/apple-touch-icon.png">
+
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link href='http://fonts.googleapis.com/css?family=Oswald:700|Open+Sans:300,700' rel='stylesheet' type='text/css'>
-    <!--<link href='http://fonts.googleapis.com/css?family=Source+Code+Pro:200,300,400,500,600,700,900|Oswald:400,700,300' rel='stylesheet' type='text/css'>-->
 
     <!-- Styles -->
-    <link href="css/all.css" rel="stylesheet">
+    <link href="/css/all.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
