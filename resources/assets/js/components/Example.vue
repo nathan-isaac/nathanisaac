85d8df5d42bcfb433ef6f6aef0abe340c775009e
Nathan Isaac <nathan@nathan-isaac.com>
2017-03-03 17:52:58 -0800

    Add basic animation to one particle

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -1,23 +1,64 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
-
-                    <div class="panel-body">
-                        I'm an example component!
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <canvas width="440" height="640" ref="canvas"></canvas>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                canvas: null,
+                ctx: null,
+                particle: {
+                    position: {
+                        x: 100,
+                        y: 100
+                    },
+                    velocity: {
+                        x: .2,
+                        y: .2
+                    },
+                    radius: 10,
+                    color: '#8BC34A'
+                }
+            }
+        },
         mounted() {
-            console.log('Component mounted.')
+            this.canvas = this.$refs.canvas;
+            this.ctx = this.canvas.getContext('2d');
+            this.drawParticle();
+            window.requestAnimationFrame(this.animateCanvas);
+        },
+        methods: {
+            drawParticle() {
+                this.ctx.beginPath();
+                this.ctx.arc(this.particle.position.x, this.particle.position.y, this.particle.radius, 0, Math.PI * 2, true);
+                this.ctx.closePath();
+                this.ctx.fillStyle = this.particle.color;
+                this.ctx.fill();
+            },
+            animateCanvas() {
+                this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+
+                this.drawParticle();
+
+                this.particle.position.x += this.particle.velocity.x;
+                this.particle.position.y += this.particle.velocity.y;
+
+                if (this.particle.position.y + this.particle.velocity.y > this.canvas.height || this.particle.position.y + this.particle.velocity.y < 0) {
+                    this.particle.velocity.y = -this.particle.velocity.y;
+                }
+
+                if (this.particle.position.x + this.particle.velocity.x > this.canvas.width || this.particle.position.x + this.particle.velocity.x < 0) {
+                    this.particle.velocity.x = -this.particle.velocity.x;
+                }
+
+                window.requestAnimationFrame(this.animateCanvas);
+            }
         }
     }
 </script>
+
+<style lang="sass" rel="stylesheet/sass">
+    canvas
+        border: 5px solid #8BC34A
+</style>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('canvas', function () {
+    return view('canvas');
+});
